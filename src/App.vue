32e4a136da9995c27a6189c177cbb5bad64a3675
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2024-11-15 16:29:21 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div class="app-wrapper">
+	</div>
+</template>
+
+<style scoped>
+	.app-wrapper {
+		width: 100vw;
+		/** Using larger height than viewport to demonstrate the scroll bug */
+		height: 120vh;
+	}
+</style>
